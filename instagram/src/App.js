af6e0a90dea0ed952e0737c65f142b482a378296
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-02-19 22:34:47 -0800

    fought with node server hot refresh and yarn corruption.  Had to delete and recreate quite a bit of my work.  Sorry if the commit history winds up a bit weird because of that.  Added post like feature and add a comment feature to posts.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import dummyData from './dummy-data';
+import uuidv4 from "uuid/v4";
 
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
@@ -9,15 +10,25 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      data: dummyData,
+      data: [],
     }
+  }
+
+  componentDidMount() {
+    this.setState({ data: dummyData })
   }
 
   render() {
     return (
       <div className="App">
         <SearchBar />
-        <PostContainer data={this.state.data}/>
+        <div className="post-container">
+          {this.state.data.map((data) => {
+            return (
+              <PostContainer key={uuidv4()} postData={data} />
+            );
+          })}
+        </div>
       </div>
     );
   }
